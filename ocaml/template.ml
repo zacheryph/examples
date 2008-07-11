c7265c56c7f23ac6eb4a0743ce2a143fd97dc768
Zachery Hostens <zacheryph@gmail.com>
2008-07-11 14:55:04 -0500

    add template idea (not code)

---
+++ ocaml/template.ml
@@ -0,0 +1,7 @@
+(* tcl like templating *)
+(*
+ * Jeff_123| a function that takes a string and a map of strings to integers, strings etc
+ * Jeff_123| the string coiuld be like
+ * Jeff_123| "%s{name}'s phone number is %i{phonenumber}."
+ * Jeff_123| and then substituted in the right values from the map
+ *)
