445d673125c1d59827ffd4f260678d86e83ada96
Zachery Hostens <zacheryph@gmail.com>
2018-05-30 23:58:44 -0500

    switch to tuple just cause
    Signed-off-by: Zachery Hostens <zacheryph@gmail.com>

--- rust/fizzbuzz/src/main.rs
+++ rust/fizzbuzz/src/main.rs
@@ -1,22 +1,19 @@
 /// My fizzbuzz for rust cause...
-
 use std::env::args;
 use std::fmt::{Display, Error, Formatter};
 use std::ops::Add;
 
-struct FizzBuzz {
-    value: i32,
-}
+struct FizzBuzz(i32);
 
 impl FizzBuzz {
     fn new(n: i32) -> FizzBuzz {
-        FizzBuzz { value: n }
+        FizzBuzz(n)
     }
 }
 
 impl Display for FizzBuzz {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        match (self.value, self.value % 3, self.value % 5) {
+        match (self.0, self.0 % 3, self.0 % 5) {
             (_, 0, 0) => write!(f, "FizzBuzz"),
             (_, 0, _) => write!(f, "Fizz"),
             (_, _, 0) => write!(f, "Buzz"),
@@ -29,9 +26,7 @@ impl Add<i32> for FizzBuzz {
     type Output = FizzBuzz;
 
     fn add(self, other: i32) -> FizzBuzz {
-        FizzBuzz {
-            value: self.value + other,
-        }
+        FizzBuzz(self.0 + other)
     }
 }
 
