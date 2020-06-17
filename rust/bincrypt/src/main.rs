edeb99a288ea4b7caedd7d87eb3f2a6a8deacbaa
Zachery Hostens <zacheryph@gmail.com>
2020-06-16 22:59:56 -0500

    replace bloaty deps with leaner ones
    Signed-off-by: Zachery Hostens <zacheryph@gmail.com>

--- rust/bincrypt/src/main.rs
+++ rust/bincrypt/src/main.rs
@@ -6,25 +6,47 @@ mod bincrypt;
 mod conf;
 
 use conf::{BinConfig, Config, CONFIG};
+use gumdrop::Options;
 use std::path::PathBuf;
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
-#[structopt(name = "bincrypt", about = "Example binary embedded configuration")]
+#[derive(Debug, Options)]
+// #[structopt(name = "bincrypt", about = "Example binary embedded configuration")]
 struct Cmd {
-    #[structopt(long)]
+    #[options(help = "print help")]
+    help: bool,
+    #[options(help = "display version info")]
+    version: bool,
+
+    #[options(no_short, help = "print config template")]
     template: bool,
 
-    #[structopt(value_name = "file", long, parse(from_os_str))]
+    #[options(
+        no_short,
+        parse(from_str),
+        meta = "FILE",
+        help = "update embedded config"
+    )]
     write: Option<PathBuf>,
 }
 
 fn main() {
-    let args = Cmd::from_args();
+    let args = Cmd::parse_args_default_or_exit();
+    if args.version {
+        println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        return;
+    }
+    if args.help_requested() {
+        println!("{}", Cmd::usage());
+        return;
+    }
+
     let settings = CONFIG.decode();
 
     if args.template {
-        println!("{}", toml::to_string_pretty(&Config::default()).unwrap());
+        println!(
+            "{}",
+            serde_json::to_string_pretty(&Config::default()).unwrap()
+        );
         return;
     }
 
@@ -32,14 +54,14 @@ fn main() {
         write_settings(&args.write.unwrap())
     } else {
         println!("Current Settings:");
-        let conf = toml::to_string_pretty(&settings.config).unwrap();
+        let conf = serde_json::to_string_pretty(&settings.config).unwrap();
         println!("{}", conf);
     }
 }
 
 fn write_settings(conf_file: &PathBuf) {
     let buf = std::fs::read(conf_file).unwrap();
-    let conf: Config = toml::from_slice(&buf).unwrap();
+    let conf: Config = serde_json::from_slice(&buf).unwrap();
     let binconf = BinConfig {
         pack_hash: [1; 20],
         local_hash: [2; 20],
